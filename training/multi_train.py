e10e138f05adf400bf137db257a4cff458d1d136
Ismail-shaik786 <isk553411@gmail.com>
2026-06-03 10:08:06 +0530

    model training is completed

--- training/multi_train.py
+++ training/multi_train.py
@@ -14,7 +14,7 @@ from sklearn.metrics import (
 
 import joblib
 
-# Load all CSV files
+
 files = glob.glob("../dataset/*.csv")
 
 dataframes = []
@@ -25,7 +25,7 @@ for file in files:
     try:
         df = pd.read_csv(file)
 
-        # Clean column names
+       
         df.columns = df.columns.str.strip()
 
         dataframes.append(df)
@@ -35,31 +35,31 @@ for file in files:
     except Exception as e:
         print(f"Error loading {file}: {e}")
 
-# Combine all datasets
+
 data = pd.concat(dataframes, ignore_index=True)
 
 print("\nAll Datasets Combined Successfully")
 
-# Replace Infinity values
+
 data.replace([np.inf, -np.inf], np.nan, inplace=True)
 
-# Remove NaN rows
+
 data.dropna(inplace=True)
 
 print("\nDataset Cleaned Successfully")
 
-# Show labels
+
 print("\nAttack Labels:")
 print(data['Label'].value_counts())
 
-# Encode labels
+
 encoder = LabelEncoder()
 
 data['Label'] = encoder.fit_transform(data['Label'])
 
 print("\nLabels Encoded Successfully")
 
-# Features and labels
+
 X = data.drop('Label', axis=1)
 
 y = data['Label']
@@ -67,7 +67,6 @@ y = data['Label']
 print("\nFeatures Shape:", X.shape)
 print("Labels Shape:", y.shape)
 
-# Split dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -77,7 +76,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("\nTraining AI Model...\n")
 
-# Train model
 model = RandomForestClassifier(
     n_estimators=100,
     random_state=42,
@@ -88,24 +86,24 @@ model.fit(X_train, y_train)
 
 print("Model Training Completed")
 
-# Predictions
+
 y_pred = model.predict(X_test)
 
-# Accuracy
+
 accuracy = accuracy_score(y_test, y_pred)
 
 print("\nModel Accuracy:")
 print(accuracy)
 
-# Classification Report
+
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
 
-# Confusion Matrix
+
 print("\nConfusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 
-# Save model
+
 joblib.dump(model, "../models/multi_ids_model.pkl")
 
 print("\nMulti-Class IDS Model Saved Successfully")
